d77fb1bd229f5c18c7371f4550c8ced96f1dfe49
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-15 16:43:53 -0600

    Added initial html. Added Audience, purpose and goal statements.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<title>Joseph Isbell - Personal Website</title>
+	</head>
+	<body>
+		<h1>PWP - MILESTONE 1</h1>
+		<hr />
+		<h2>Purpose</h2>
+		<p>The purpose of this website it to showcase my portfolio which focuses on mostly front and some back-end development.</p>
+		<h2>Audience</h2>
+			<p>This project will target a range of developers, peers, clients and employers in the web-development and
+				technology industries</p>
+		<h2>Goal</h2>
+			<p>The main goal of this project is to initiate contact with potential employers, collaborators and
+			clients through the contact links on the webpage. The other goal of this project is to showcase my skills and
+			abilities in professional web development</p>
+
+	</body>
+</html>
