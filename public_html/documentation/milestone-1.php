ae60d94ba7a04d3d77b933a6e1611b6e715f572b
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-15 17:45:43 -0600

    Added attitude, needs, goals and user story. Added comments above each subsection.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -26,12 +26,34 @@
 				<p>Owen Moore</p>
 			<h3>Age: </h3>
 				<p>32</p>
-			<h3>Profession</h3>
+			<!-- Persona's Profession and technology comfort -->
+			<h3>Profession: </h3>
 			<p>Owen recently founded a popular start-up called Raven, Inc. which provides professional, front-end websites
 				for a wide-range of small and medium-scale companies in various industries</p>
 			<h3>Technology</h3>
 			<p>He is very comfortable with technology and owns a 13.3" MacBook Air with an i5 processor along with an
 				iPhone X with an unlimted 4G connection. He always tries to get the latest versions if possible.</p>
+			<!-- Attitude and Needs in a website -->
+			<h3>Attitude: </h3>
+			<p>Owen is dedicated to growing his business and making it succeed. He has a laid-back attitude but expects
+				himself and his employees to be professional and work hard to meet deadlines. He likes to have fun and goof off, but treats his clients and
+				employees with respect. Owen is a huge social media user and utilizes sites such as Twitter and LinkedIn on a daily basis for networking purposes.</p>
+			<h3>Needs and Frustrations: </h3>
+			<p>Owen is often too busy trying to meet deadlines and cannot spend more than a few minutes looking at a
+				potential employee's resume/portfolio. He needs to be able to determine quickly if this person, will fit in with the rest of the team, has the
+				skills and experience necessary to join his company and keep up with the demands of the clients assigned. He
+				wants to be able to quickly view their portfolio and examine their previous work through Github. He also,
+				wants to view their LinkedIn and Twitter profiles to see what they are following professionally and what kind of personality they have.</p>
+			<!-- Persona's Goals -->
+			<h3>Goals: </h3>
+			<p>Owen's goals are to determine if the person he is considering can fit in well with his team and keep up with
+				the demands of the job. Can he quickly veiw this person's previous work and make a decision to pursue
+				following up with them? He wants to know if this person will help his company grow without spending more
+				than a few minutes looking at their work/social media accounts.</p>
+			<!-- One sentence User Story to sum up Persona's reason for using the site -->
+			<h3>User Story</h3>
+			<p>Owen wants to quickly determine whether a candidate has the skills and attitude necessary to join his team
+				and meet client demands.</p>
 		</div>
 	</body>
 </html>
