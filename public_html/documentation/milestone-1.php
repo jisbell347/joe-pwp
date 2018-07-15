9898efc59359d08948f7e385383252fd136d3f8f
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-15 16:50:35 -0600

    Added comments to break up the sections.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,16 +6,21 @@
 	</head>
 	<body>
 		<h1>PWP - MILESTONE 1</h1>
-		<hr />
-		<h2>Purpose</h2>
-		<p>The purpose of this website it to showcase my portfolio which focuses on mostly front and some back-end development.</p>
-		<h2>Audience</h2>
-			<p>This project will target a range of developers, peers, clients and employers in the web-development and
-				technology industries</p>
-		<h2>Goal</h2>
-			<p>The main goal of this project is to initiate contact with potential employers, collaborators and
-			clients through the contact links on the webpage. The other goal of this project is to showcase my skills and
-			abilities in professional web development</p>
+		<!-- Audience, Purpose and Goal of the website -->
+		<div>
+			<h2>Purpose</h2>
+			<p>The purpose of this website it to showcase my portfolio which focuses on mostly front and some back-end development.</p>
+			<h2>Audience</h2>
+				<p>This project will target a range of developers, peers, clients and employers in the web-development and
+					technology industries</p>
+			<h2>Goal</h2>
+				<p>The main goal of this project is to initiate contact with potential employers, collaborators and
+				clients through the contact links on the webpage. The other goal of this project is to showcase my skills and
+				abilities in professional web development</p>
+		</div>
+		<!-- Persona -->
+		<div>
 
+		</div>
 	</body>
 </html>
