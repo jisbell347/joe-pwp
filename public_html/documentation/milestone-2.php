5434af1235c5c8b515867282194ea6d462f13a32
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-18 14:46:52 -0600

    Changed h2 tags to h3 for cleaner look

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,29 +8,29 @@
 		<h1>Milestone 2 Content Strategy</h1>
 		<p>My site will be a landing page that will feature the following sections</p>
 		<div>
-			<h2>Welcome</h2>
+			<h3>Welcome</h3>
 			<p>A welcome message about myself and my role in professional web development. The message will be displayed over a large background image of a workspace</p>
 		</div>
 		<div>
-			<h2>About Me</h2>
+			<h3>About Me</h3>
 			<p>This section will display my professional coding history, skill set and accomplishments. I will use it to
 				showcase my abilities and personality along with my strengths.  It will also feature a sub section that will
 				display the skills and programming languages I have learned in an easy-to-read format. The tone of voice in
 				this section will be warm and casual, as I am targeting opportunities and individuals that I am a good personality match for.</p>
 		</div>
 		<div>
-			<h2>Portfolio</h2>
+			<h3>Portfolio</h3>
 			<p>This portfolio will ideally be a responsive image gallery on mobile devices and an image carosel on desktop
 				that features 4-6 examples of my work. The examples will open a modal window when clicked that will display
 				my role and contributions to each project with references to the technology used.</p>
 		</div>
 		<div>
-			<h2>Contact</h2>
+			<h3>Contact</h3>
 			<p>The contact section will be a contact form with options to leave a name, email address and a short message
 				if desired. It will also include links to my email address and professional social media profiles: Github,
 				Linkedin, and Twitter. All of my social media profiles will also be added to the footer section of the page via icons.</p>
 		</div>
-		<h2>Wireframe Mockup</h2>
+		<h3>Wireframe Mockup</h3>
 		<p>Featured is a conceptual model for what I envision the final product to look like with some possible minor changes. A mobile version and a desktop version are displayed. The wireframes were made using
 			<a href="https://balsamiq.com">Balsamiq</a></p>
 		<div>
