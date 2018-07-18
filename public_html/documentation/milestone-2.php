841bc75f9f00d9389d3e3c996263ca0aa84bebdd
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-18 11:50:39 -0600

    Created about me section

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -8,7 +8,14 @@
 		<h1>Milestone 2 Content Strategy</h1>
 		<div>
 			<h2>Welcome</h2>
-			<p>A welcome message about myself and my role in web development. The message will be displayed over a large background image of my workspace</p>
+			<p>A welcome message about myself and my role in web development. The message will be displayed over a large background image of a workspace</p>
+		</div>
+		<div>
+			<h2>About Me</h2>
+			<p>This section will display my professional coding history, skill set and accomplishments. I will use it to
+				showcase my abilities and personality along with my strengths.  It will also feature a sub section that will
+				display the skills and programming languages I have learned in an easy-to-read format. The tone of voice in
+				this section will be warm and casual, as I am targeting opportunities and individuals that I am a good personality match for.</p>
 		</div>
 
 	</body>
