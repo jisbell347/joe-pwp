c30dfa7bc342152ca753dfd05c45a8a68233502e
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-18 11:41:37 -0600

    Added boilerplate html and welcome section

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Pwp Milestone 2a</title>
+	</head>
+	<body>
+		<h1>Milestone 2 Content Strategy</h1>
+		<div>
+			<h2>Welcome</h2>
+			<p>A welcome message about myself and my role in web development. The message will be displayed over a large background image of my workspace</p>
+		</div>
+
+	</body>
+</html>
