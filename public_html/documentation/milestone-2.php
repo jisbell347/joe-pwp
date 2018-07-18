61c0bd3c1273905220958fed8822e9ba320039b4
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-18 14:34:06 -0600

    Added portfolio, contact and wireframes sections

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -9,7 +9,7 @@
 		<p>My site will be a landing page that will feature the following sections</p>
 		<div>
 			<h2>Welcome</h2>
-			<p>A welcome message about myself and my role in web development. The message will be displayed over a large background image of a workspace</p>
+			<p>A welcome message about myself and my role in professional web development. The message will be displayed over a large background image of a workspace</p>
 		</div>
 		<div>
 			<h2>About Me</h2>
@@ -17,6 +17,29 @@
 				showcase my abilities and personality along with my strengths.  It will also feature a sub section that will
 				display the skills and programming languages I have learned in an easy-to-read format. The tone of voice in
 				this section will be warm and casual, as I am targeting opportunities and individuals that I am a good personality match for.</p>
+		</div>
+		<div>
+			<h2>Portfolio</h2>
+			<p>This portfolio will ideally be a responsive image gallery on mobile devices and an image carosel on desktop
+				that features 4-6 examples of my work. The examples will open a modal window when clicked that will display
+				my role and contributions to each project with references to the technology used.</p>
+		</div>
+		<div>
+			<h2>Contact</h2>
+			<p>The contact section will be a contact form with options to leave a name, email address and a short message
+				if desired. It will also include links to my email address and professional social media profiles: Github,
+				Linkedin, and Twitter. All of my social media profiles will also be added to the footer section of the page via icons.</p>
+		</div>
+		<h2>Wireframe Mockup</h2>
+		<p>Featured is a conceptual model for what I envision the final product to look like with some possible minor changes. A mobile version and a desktop version are displayed. The wireframes were made using
+			<a href="https://balsamiq.com">Balsamiq</a></p>
+		<div>
+			<div>
+				<img src="../images/mobile-mockup.png" alt="Mockup of Website for Mobile Version" />
+			</div>
+			<div>
+				<img src="../images/desktop-mockup.png" alt="Mockup of Website for Desktop Version" />
+			</div>
 		</div>
 
 	</body>
