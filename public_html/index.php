c84c005cee6750cbf0ee6142e6361c564b81d1c7
Joseph Isbell <shadowwolf347@comcast.net>
2018-08-06 15:04:49 -0600

    Added script tags for form validation

--- public_html/index.php
+++ public_html/index.php
@@ -10,8 +10,17 @@
 		<link rel="stylesheet" href="css/style.css" type="text/css">
 		<!-- JQuery first, then Popper.js, then Bootstrap.js -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.17.0/additional-methods.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+		<!-- Javascript Form Validator -->
+		<script src="js/form-validate.js"></script>
+
+		<!-- Google reCAPTCHA -->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
 		<!--Title of project -->
 		<title>Joseph Isbell's Portfolio</title>
 	</head>
@@ -201,7 +210,7 @@
 				<h4 class="display-4">Contact</h4>
 			</div>
 			<!-- TODO: fix form for mailer -->
-			<form id="contact-form" action="php/mailer.php" method="post" class="col-md-8 offset-2 mx-auto">
+			<form id="contact-form" action="php/mail-config.php" method="post" class="col-md-8 offset-2 mx-auto">
 				<div class="form-group">
 					<label for="contactFormName">Name</label>
 					<div class="input-group">
